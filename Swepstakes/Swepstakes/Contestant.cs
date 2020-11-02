f6734d7f4dbc8bbeb6575cb36dbfaf03e7a3f5a5
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-11-02 13:58:36 -0600

    contestant class

---
+++ Swepstakes/Swepstakes/Contestant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swepstakes
+{
+    class Contestant
+    {
+        string firstName;
+        string lastName;
+        string email;
+        int registrationNumber;
+    }
+}
